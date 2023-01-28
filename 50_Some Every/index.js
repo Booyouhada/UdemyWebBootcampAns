71b6a2628f00c5999741befde698bbd904e04901
Joy Kim <booyouhada@gmail.com>
2023-01-28 10:13:28 +0900

    answer added

---
+++ 50_Some Every/index.js
@@ -0,0 +1,10 @@
+/*  const allEvens = function(nums) {
+    return nums.every(function(num){
+        return num % 2 === 0;
+    });
+}
+*/
+
+const allEvens = nums => {
+    return nums.every(num => num % 2 === 0);
+}
