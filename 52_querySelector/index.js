606c54d3a368d87ed96b0ce94d746c53a06f5ce3
Joy Kim <booyouhada@gmail.com>
2023-01-29 13:48:03 +0900

    answer added

--- 52_querySelector/index.js
+++ 52_querySelector/index.js
@@ -1,3 +1,5 @@
 // Your code goes in here!
 const doneTodos = document.querySelectorAll('.done');
 const checkbox = document.querySelector('input:nth-of-type(2)');
+// const checkbox = document.querySelctor('input[type = "checkbox"]');
+
