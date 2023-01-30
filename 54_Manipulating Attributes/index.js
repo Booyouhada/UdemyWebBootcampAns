6e86a61df167ffdc7008ce736a46b7e57d31ce3e
Joy Kim <booyouhada@gmail.com>
2023-01-31 00:02:33 +0900

    answer added

---
+++ 54_Manipulating Attributes/index.js
@@ -0,0 +1,5 @@
+// The url you need: 'https://devsprouthosting.com/images/chicken.jpg'
+const img = document.querySelector("img")
+img.setAttribute("src", "https://devsprouthosting.com/images/chicken.jpg");
+img.alt = "chicken";
+`
