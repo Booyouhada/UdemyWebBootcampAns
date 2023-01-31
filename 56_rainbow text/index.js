0914731434c349215b3311233141378a473d28a8
Joy Kim <booyouhada@gmail.com>
2023-01-31 15:38:51 +0900

    answer added

---
+++ 56_rainbow text/index.js
@@ -0,0 +1,9 @@
+const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']; //PLEASE DON'T CHANGE THIS LINE!
+
+//YOU CODE GOES HERE:
+const spans = document.querySelectorAll("span");
+let i = 0;
+for (span of spans) {
+    span.style.color = colors[i];
+    i++;
+}
