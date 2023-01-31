6a23b7ca1ab1301b8cdeffb783dc6e87260acc08
Joy Kim <booyouhada@gmail.com>
2023-01-31 12:07:33 +0900

    answer added

---
+++ 55_Magical Forest Circle/index.js
@@ -0,0 +1,11 @@
+/*document.querySelector("#container").style.textAlign = "center";
+document.querySelector("img").style.width = "150px";
+document.querySelector("img").style.borderRadius = "50%";
+*/
+
+const container = document.querySelector('#container');
+const img = document.querySelector('img');
+
+container.style.textAlign = 'center';
+img.style.width = '150px';
+img.style.borderRadius = '50%';
