ef02113ff9918ef78c16292a927ca8698cb669e5
Joy Kim <booyouhada@gmail.com>
2023-02-02 12:41:35 +0900

    answer added

--- 52_querySelector/app.js
+++ 52_querySelector/app.js
@@ -1,5 +1,4 @@
 // Your code goes in here!
 const doneTodos = document.querySelectorAll('.done');
 const checkbox = document.querySelector('input:nth-of-type(2)');
-// const checkbox = document.querySelctor('input[type = "checkbox"]');
-
+// const checkbox = document.querySelctor('input[type = "checkbox"]');`
--- 54_Manipulating Attributes/app.js
+++ 54_Manipulating Attributes/app.js
@@ -2,4 +2,3 @@
 const img = document.querySelector("img")
 img.setAttribute("src", "https://devsprouthosting.com/images/chicken.jpg");
 img.alt = "chicken";
-`
