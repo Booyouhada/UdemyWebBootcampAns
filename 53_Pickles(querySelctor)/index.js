1fdcaca9875acaada891ff5a9932818be4ee45c8
Joy Kim <booyouhada@gmail.com>
2023-02-02 09:22:36 +0900

    answer added

--- 53_Pickles(querySelctor)/index.js
+++ 53_Pickles(querySelctor)/index.js
@@ -1,2 +1,3 @@
 // YOUR CODE GOES IN HERE:
-document.querySelector('h1 >span').innerText = 'Disgusting';
+document.querySelector('h1 > span').innerText = 'Disgusting';
+// document.querySelector('h1 > span').textContent = 'Disgusting';
